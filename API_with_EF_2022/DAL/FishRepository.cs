255db3e4e1a18fcba570078d024dd7b88ec70e98
unknown <cjsteward07@gmail.com>
2023-02-20 21:04:01 -0500

    update

---
+++ API_with_EF_2022/DAL/FishRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace API_with_EF_2022.DAL
+{
+    public class FishRepository
+    {
+    }
+}
---
+++ API_with_EF_2022/Models/Fish.cs
@@ -0,0 +1,12 @@
+﻿namespace API_with_EF_2022.Models
+{
+    public class Fish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Classification { get; set; }
+        public bool FreshWater { get; set; }
+        public bool SaltWater { get; set; }
+
+    }
+}
